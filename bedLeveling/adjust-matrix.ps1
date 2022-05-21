0fb0c419e1ba1b616775e38dbe8f73004aa69280
Peter Lanoie <peter.lanoie@octopus.com>
2022-05-21 18:25:47 -0400

    Add matrix adjustment util

---
+++ bedLeveling/adjust-matrix.ps1
@@ -0,0 +1,215 @@
+[CmdletBinding()]
+param (
+	[int] $bedMaxX = 235,
+	[int] $bedMaxY = 235,
+	[int] $testZgap = .2,
+	[switch] $speak
+)
+
+class Offset {
+	[char] $Position
+	[int] $X
+	[int] $Y
+	[decimal] $Offset
+	[decimal] $AdjustedZ
+	Offset($position, $x, $y, $adjustedZ){
+		$this.Position = $position
+		$this.X = $x
+		$this.Y = $y
+		$this.AdjustedZ = $adjustedZ
+	}
+}
+
+[decimal]$testZgap = 0.2
+[decimal]$jogDelta = 0.01
+
+$testPoints = @(
+	[Offset]::New('0', 0, 0, $testZgap),
+	[Offset]::New('1', 0, 1, $testZgap),
+	[Offset]::New('2', 0, 2, $testZgap),
+	[Offset]::New('3', 0, 3, $testZgap),
+	[Offset]::New('4', 1, 3, $testZgap),
+	[Offset]::New('5', 1, 2, $testZgap),
+	[Offset]::New('6', 1, 1, $testZgap),
+	[Offset]::New('7', 1, 0, $testZgap),
+	[Offset]::New('8', 2, 0, $testZgap),
+	[Offset]::New('9', 2, 1, $testZgap),
+	[Offset]::New('A', 2, 2, $testZgap),
+	[Offset]::New('B', 2, 3, $testZgap),
+	[Offset]::New('C', 3, 3, $testZgap),
+	[Offset]::New('D', 3, 2, $testZgap),
+	[Offset]::New('E', 3, 1, $testZgap),
+	[Offset]::New('F', 3, 0, $testZgap)
+)
+
+function PrintHelp {
+	Write-Host "Z adjustment routine."
+	Write-Host "  - Navigate test points with [<] and [>]"
+	Write-Host "  - Go direct to test points using the following keys:"
+	Write-Host "     ------ rear ------"
+	Write-Host "     [3]  [4]  [B]  [C]"
+	Write-Host "     [2]  [5]  [A]  [D]"
+	Write-Host "     [1]  [6]  [9]  [E]"
+	Write-Host "     [0]  [7]  [8]  [F]"
+	Write-Host "     ------ front -----"
+	Write-Host "  - [Up Arrow] to raise Z by 0.01mm"
+	Write-Host "  - [Down Arrow] to lower Z by 0.01mm"
+	Write-Host "  - [Enter] to send test point adjustment (mesh Z-delta) to printer"
+	Write-Host "  - [R] to reset adjustment"
+	Write-Host "  - [T] got bed center and back to current test point"
+	Write-Host "  - [S] run test point circuit path"
+	Write-Host "  - [M] to toggle speech mode"
+	Write-Host "  - [H] print this help"
+	Write-Host "  - Press Q to quit."
+}
+
+PrintHelp
+
+$speechOn = $speak
+$speechReady = $false
+
+function Speak {
+	param ($words)
+	if($speechOn){
+		if (!$speechReady) {
+			$speaker = (New-Object -com SAPI.SpVoice)
+			$speaker.rate = 5
+		}
+		$speaker.speak($words, 1) | Out-Null
+	}
+	return $words
+}
+
+function RunJog {
+	param ([double] $zJog)
+
+	$jogPayload = @{
+		command = "jog"
+		z = $zJog
+	}
+	
+	."$PSCommandPath/../octoprint/call-api.ps1" -operation "printer/printhead" -method POST -payload $jogPayload
+}
+
+$testPointIndex = -1
+$nextTestPointIndex = 0
+
+while($keyInput.KeyChar -ne "q"){
+	if ($nextTestPointIndex -ne $testPointIndex) {
+		$currentTestPoint = $testPoints[($testPointIndex = $nextTestPointIndex)]
+		Write-Host "Going to test point $($currentTestPoint.X) $($currentTestPoint.Y)"
+		. "$PSScriptRoot\..\BedLeveling\goto-testpoint.ps1" $currentTestPoint.X $currentTestPoint.Y $testZgap -noecho -bedMaxX $bedMaxX -bedMaxY $bedMaxY
+		Speak("Test point $($currentTestPoint.X) $($currentTestPoint.Y)")
+		$pointX = $currentTestPoint.X
+		$pointY = $currentTestPoint.Y
+	}
+
+	$phrase = ""
+	Write-Host "Current status:"
+	Write-Host "   test point $($currentTestPoint.X) $($currentTestPoint.Y)"
+	Write-Host "   adjustment: $($currentTestPoint.Offset)"
+	Write-Host "   current Z: $($currentTestPoint.AdjustedZ)"
+	Write-Host "   z jog delta: $jogDelta"
+	$keyInput = [System.Console]::ReadKey($true) #$Host.UI.RawUI.ReadKey('NoEcho')
+	$direction = 0
+	switch ($keyInput.Key) {
+		"RightArrow" {
+			$jogDelta += 0.01
+			$phrase = "increase z change step"
+		}
+		"LeftArrow" {
+			if ($jogDelta -gt 0) {
+				$jogDelta -= 0.01
+				$phrase = "decrease z change step"
+			}
+		}
+		"UpArrow" { 
+			$direction = 1
+			$phrase = "up"
+		}
+		"DownArrow" { 
+			$direction = -1
+			$phrase = "down"
+		}
+		"Enter" {
+			[console]::beep(800,100)
+			Write-Host (Speak("Saving new Z adjustment")) $currentTestPoint.Offset
+#			$currentTestPoint.Offset = $zAdjustment
+			$command = "M421 I$pointX J$pointY Q$($currentTestPoint.Offset)"
+			. "$PSScriptRoot\..\octoprint\send-commands.ps1" -gcode $command
+			$currentTestPoint.Offset = 0
+			$currentTestPoint.AdjustedZ = $testZgap
+			break
+		}
+	}
+	switch -regex ($keyInput.KeyChar){
+		"r" { 
+			Write-Host "Resetting adjustment"
+			RunJog($currentTestPoint.Offset * -1)
+			$currentTestPoint.Offset = 0
+			$currentTestPoint.AdjustedZ = $testZgap
+			$phrase = "reset adjustment"
+		}
+		"q" {
+			$phrase = "quit"
+			Write-Host "Quitting"
+		}
+		"m" {
+			Write-Host (Speak("Speech is " + (($speechOn = !$speechOn) ? "on" : "off")))
+		}
+		"h" {
+			PrintHelp
+		}
+		"[n\.]" {
+			if ($testPointIndex -lt $testPoints.Length-1) {
+				$nextTestPointIndex = $testPointIndex + 1
+			}
+		}
+		"[p,]" {
+			if ($testPointIndex -gt 0) {
+				$nextTestPointIndex = $testPointIndex - 1
+			}
+		}
+		"t" {
+			. "$PSScriptRoot\..\octoprint\goto.ps1" ($bedMaxX / 2) ($bedMaxY / 2) 10 6000
+			. "$PSScriptRoot\goto-testpoint.ps1" $currentTestPoint.X $currentTestPoint.Y $testZgap -noecho -bedMaxX $bedMaxX -bedMaxY $bedMaxY
+		}
+		"s" {
+
+			foreach ($circuitPoint in $testPoints) {
+				Write-Host "Going to test point: $($circuitPoint.X) $($circuitPoint.Y)"
+				. "$PSScriptRoot\goto-testpoint.ps1" $circuitPoint.X $circuitPoint.Y $testZgap -noecho -bedMaxX $bedMaxX -bedMaxY $bedMaxY
+				Start-Sleep -Seconds 2
+			}
+			. "$PSScriptRoot\goto-testpoint.ps1" $currentTestPoint.X $currentTestPoint.Y $testZgap -noecho -bedMaxX $bedMaxX -bedMaxY $bedMaxY
+		}
+		'[0-9a-f]' {
+			for ($i = 0; $i -lt $testPoints.Count; $i++) {
+				if ($testPoints[$i].Position -eq $keyInput.KeyChar) {
+					$nextTestPointIndex = $i
+				}
+			}
+		}
+	}
+	if ($direction -ne 0) {
+		$jog = $jogDelta * $direction
+		$newAdjustment = $currentTestPoint.Offset + $jog
+		if (($testZgap + $newAdjustment) -lt 0) {
+			Write-Warning "Z is at 0, cannot adjust further"
+			$newAdjustment = 0
+		} else {
+			Write-Host "Jogging Z by $jog"
+			RunJog($jog)
+			$currentTestPoint.Offset = $newAdjustment
+			$currentTestPoint.AdjustedZ = $testZgap + $currentTestPoint.Offset
+		}
+	}
+#	[console]::beep(400,100)
+	if ($phrase.Length -gt 0) {
+		Speak($phrase) | Out-Null
+	}
+	
+	#	Write-Host $direction
+}
+
+#$testPoints | Format-Table
---
+++ bedLeveling/goto-testpoint.ps1
@@ -0,0 +1,46 @@
+[CmdletBinding()]
+param (
+	[Parameter(Position=0)]
+	[int] $gridPointX,
+
+	[Parameter(Position=1)]
+	[int] $gridPointY,
+
+	[Parameter(Position=2)]
+	[double] $z = 0.2,
+
+	[int] $bedMaxX = 235,
+	[int] $bedMaxY = 235,
+
+	[int] $testGridXpoints = 4,
+	[int] $testGridYpoints = 4,
+
+	[switch] $noecho
+)
+
+$xGridPoints = New-Object decimal[] $testGridXpoints
+$yGridPoints = New-Object decimal[] $testGridYpoints
+
+$gridPortionX = $bedMaxX / ($testGridXpoints - 1)
+$gridPortionY = $bedMaxY / ($testGridYpoints - 1)
+
+for ($i = 0; $i -lt $testGridXpoints; $i++) {
+	$xGridPoints[$i] = [System.Math]::Round($i * $gridPortionX)
+}
+
+for ($i = 0; $i -lt $testGridYpoints; $i++) {
+	$yGridPoints[$i] = [System.Math]::Round($i * $gridPortionY)
+}
+
+# $xGridPoints = @(0, 78, 156, 235)
+# $yGridPoints = @(0, 78, 156, 235)
+
+$x = $xGridPoints[$gridPointX];
+$y = $yGridPoints[$gridPointY];
+
+$commands = "
+G0 Z1 F6000
+G0 X$x Y$y F6000
+G0 Z$z F500"
+
+."$PSScriptRoot\..\octoprint\send-commands.ps1" $commands -echo:(!$noecho) -noinfo
