f5a0663d37885e65e8a64429d061806c81c2c1b6
Peter Lanoie <peter.lanoie@octopus.com>
2022-07-16 12:33:50 -0400

    Refactor: optimize file read strategy

--- scripts/gcode/.functions.ps1
+++ scripts/gcode/.functions.ps1
@@ -18,6 +18,8 @@ function Update-File {
 	Write-Host "Processing file: $infile"
 	Write-Host "-----------------------------------------------------------"
 
+	$startTime = Get-Date
+
 	if ($infile -is [String]) {
 		$infile = Get-ChildItem $infile
 	}
@@ -45,18 +47,40 @@ function Update-File {
 	}
 	
 	Write-Host "Generating new file: $outFileName"
-	
-	[string[]]$filelines = Get-Content $infile
-	$newLines = [System.Text.StringBuilder]::new()
 
-	$lineCount = $filelines.Count
+	$tempOutFile = $outFileName + ".temp"
+	if(Test-Path -Path $tempOutFile){
+		Remove-Item -Path $tempOutFile -Force
+	}
+	
+#	$fileSize = (Get-Item $infile).length
+	$processedLength = 0
+	$layerCount = 0
+	$lineCount = 0
+
+	$lineIterator = [System.IO.File]::ReadLines($infile)
+
+	Write-Host "Doing file prescan..."
+	foreach ($line in $lineIterator) {
+		$lineCount++
+		if ($line.StartsWith(";LAYER:")) {
+			$layerCount++
+		}
+	}
+	Write-Host "Found $layerCount layers in $lineCount lines"
+#	$lineIterator.Reset()
+
 	$actionArgs = New-Object -TypeName LineActionArgs
 
-	for ($i = 0; $i -lt $lineCount ; $i++) {
-		$actionArgs.lineNumber = $i
+	$newLines = [System.Text.StringBuilder]::new()
+	# $flushThreshold = 50 * 1024
+	$i = 0
+	foreach ($line in $lineIterator) {
+		$actionArgs.lineNumber = $i++
 		$actionArgs.isNewLayer = $false
-		Write-Progress -Activity "Processing GCode lines" -PercentComplete ($i / $lineCount * 100)
-		$actionArgs.lineText = $line = $filelines[$i]
+		$actionArgs.lineText = $line # = $filelines[$i]
+		$processedLength += $line.Length + 1
+		Write-Progress -Activity "Processing GCode lines ($i)" -PercentComplete ($i / $lineCount * 100)
 
 		if ($line.StartsWith(";LAYER:")) {
 			$actionArgs.layerNumber = [int]$line.Replace(";LAYER:", "") + 1
@@ -64,10 +88,23 @@ function Update-File {
 			$actionArgs.isNewLayer = $true
 		}
 		$resultLines = 	$updateAction.Invoke([LineActionArgs]$actionArgs) #
-		if ($resultLines) {
+		#if ($resultLines) {
 			$resultLines | ForEach-Object { $newLines.AppendLine($_) | Out-Null }
-		}
+		#}
+		# flush new line buffer every 10k chars
+
+		# if ($newLines.Length -gt $flushThreshold) {
+		# 	[System.IO.File]::AppendAllText($tempOutFile, $newLines)
+		# 	$newLines.Clear() | Out-Null
+		# }
 	}
-	
-	Set-Content -Path $outFileName -Value $newLines
+	#final flush
+	[System.IO.File]::AppendAllText($tempOutFile, $newLines)
+
+	Move-Item -Path $tempOutFile -Destination $outFileName -Force
+#	Set-Content -Path $outFileName -Value $newLines
+
+	$endTime = Get-Date
+	$elapsedTime = $endTime - $startTime
+	Write-Host "Process Time: $elapsedTime"
 }
